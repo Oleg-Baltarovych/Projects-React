7f1af733bfc5ab4bd73cb04bc62a5310b3187cd5
Oleg-Baltarovych <69357715+Oleg-Baltarovych@users.noreply.github.com>
2022-07-31 18:13:53 +0300

    Add files via upload

---
+++ Front/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import RecordList from "./components/RecordList";
+
+function App() {
+  return (
+    <div>
+      <RecordList />
+    </div>
+  );
+}
+
+export default App;
